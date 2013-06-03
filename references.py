bf454375b600bbc9a5adaa506636cc5da16ff982
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-06-03 10:27:46 +0300

    large dictionary of Wikidata item IDs for Wikipedia language versions
    importable, for use with pywikibot's `reference.setTarget`

---
+++ references.py
@@ -0,0 +1,34 @@
+"""
+Large dictionary of Wikidata item IDs for Wikipedia language versions
+(not sure if it can be copyrighted)
+"""
+languages={
+  'ar' : 199700,
+	'ceb': 837615,
+	'da' : 181163,
+	'de' : 48183,
+	'en' : 328,
+	'fi' : 175482,
+	'fr' : 8447,
+	'el' : 11918,
+	'eo' : 190551,
+	'es' : 8449,
+	'gl' : 841208,
+	'he' : 199913,
+	'ia' : 3757068,
+	'ie' : 6167360,
+	'io' : 1154766,
+	'it' : 11920,
+	'ja' : 177837,
+	'mk' : 842341,
+	'nl' : 10000,
+	'pl' : 1551807,
+	'pt' : 11921,
+	'ru' : 206855,
+	'tr' : 58255,
+	'sv' : 169514,
+	'uk' : 199698,
+	'vi' : 200180,
+	'vo' : 714826,
+	'zh' : 30239
+}
