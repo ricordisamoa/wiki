70e83a9fb31649804019c9c78c4415ffb0692394
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-06-23 14:59:51 +0200

    enhance scalability

--- add_section.py
+++ add_section.py
@@ -2,17 +2,40 @@
 
 import pywikibot
 
-def insert(site=pywikibot.Site('it','wikipedia'),title='Wikipedia:Vandalismi in corso'):
-	header=pywikibot.data.api.Request(site=site,action='expandtemplates',text='{{#timel:d xg}}').submit()['expandtemplates']['*']
-	page=pywikibot.Page(site,title)
+pages={
+	'wikipedia':{
+		'it':[
+			{
+				'title':'Wikipedia:Vandalismi in corso',
+				'level':3
+			}
+		]
+	}
+}
+
+def main():
+	for family in pages:
+		for lang in pages[family]:
+			for page in pages[family][lang]:
+				kwargs=page
+				kwargs['page']=pywikibot.Page(pywikibot.Site(lang,family),kwargs['title'])
+				del kwargs['title']
+				insert(**kwargs)
+
+def insert(page,level=2,space=True,minorEdit=True):
+	header=pywikibot.data.api.Request(site=page.site,action='expandtemplates',text='{{#timel:d xg}}').submit()['expandtemplates']['*']
 	text=page.get(force=True)
 	if pywikibot.textlib.does_text_contain_section(text,header):
 		pywikibot.output(u'\03{{lightyellow}}{page} already contains the header for "{header}"\03{{default}}'.format(page=page,header=header))
 		return
-	page.text+=u'\n\n== {header} =='.format(header=header)
+	if isinstance(level,int):
+		level='='*level
+	if isinstance(space,bool):
+		space=(' ' if space==True else '')
+	page.text+=u'\n\n{level}{space}{header}{space}{level}'.format(header=header,level=level,space=space)
 	pywikibot.showDiff(text,page.text)
-	page.save(comment='[['+site.namespace(4)+':Bot|Bot]]: inserimento della sezione giornaliera',minor=True,botflag=True)
+	page.save(comment='[['+page.site.namespace(4)+':Bot|Bot]]: inserimento della sezione giornaliera',minor=minorEdit,botflag=True)
 
 if __name__=='__main__':
 	pywikibot.handleArgs()
-	insert()
+	main()
