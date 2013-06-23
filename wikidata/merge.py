1c1132bafadae57917e9dc7c3cb008063d6ef2bc
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-06-23 13:20:55 +0200

    alert on conflicting sitelinks

--- wikidata/merge.py
+++ wikidata/merge.py
@@ -22,6 +22,15 @@ def merge_items(item1,item2,force_lower=True,taxon_mode=True):
 		del_msg(item2)
 		return
 	pywikibot.output(u'merging {item1} with {item2}'.format(item1=item1.getID(),item2=item2.getID()))
+	item1.get(force=True)
+	item2.get(force=True)
+	for key in item1.sitelinks:
+		if key in item2.sitelinks and item2.sitelinks[key]!=item1.sitelinks[key]:
+			pywikibot.output('\03{{lightred}}{key} sitelink conflicting: {sl1} in {item1} but {sl2} in {item2}\03{{default}}'.format(key=key,sl1=item1.sitelinks[key],sl2=item2.sitelinks[key],item1=item1.getID(),item2=item2.getID()))
+			return False
+	new_sitelinks=item1.sitelinks
+	new_sitelinks.update(item2.sitelinks)
+	pywikibot.output(new_sitelinks)
 	# TODO: implement the actual merging mechanism
 
 def main(source='User:Soulkeeper/dups',sourcetype=list,taxon_mode=True):
