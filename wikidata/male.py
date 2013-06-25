b844689fdb51b835345b7a3487a7780e7c6cf39e
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-06-26 00:46:05 +0200

    minor fixes

--- wikidata/male.py
+++ wikidata/male.py
@@ -28,8 +28,10 @@ def log(title,item,text=''):
 	pywikibot.output(u'\03{{lightyellow}}item {qid}{text}\03{{default}}'.format(qid=item.getID(),text=text))
 	page = pywikibot.Page(site,title)
 	page.get(force=True)
-	page.text += '\n'+u'*{{{{Q|{qid}}}}}'.format(qid=item.getID())+text
-	page.save(u'[[Wikidata:Bots|Bot]]: [[{qid}]]{text}'.format(qid=item.getID(),text=text),minor=True,botflag=True)
+	add = u'*{{{{Q|{qid}}}}}'.format(qid=item.getID().replace('q',''))+text
+	if not add in page.text:
+		page.text += '\n'+add
+		page.save(u'[[Wikidata:Bots|Bot]]: [[{qid}]]{text}'.format(qid=item.getID(),text=text),minor=True,botflag=True)
 
 for line in lines:
 	item = pywikibot.ItemPage(site,line)
@@ -43,10 +45,10 @@ for line in lines:
 		item.get(force=True)
 	if 'en' in item.labels:
 		if re.search(ur'\bthe\b',item.labels['en']):
-			log('User:SamoaBot/sex doubts',item,u'contains "the" in English label: "{label}"'.format(label=item.labels['en']))
+			log('User:SamoaBot/sex doubts',item,u' contains "the" in English label: "{label}"'.format(label=item.labels['en']))
 			continue
 		if re.search(ur'\band\b',item.labels['en']):
-			log('User:SamoaBot/sex doubts',item,u'contains "and" in English label: "{label}"'.format(label=item.labels['en']))
+			log('User:SamoaBot/sex doubts',item,u' contains "and" in English label: "{label}"'.format(label=item.labels['en']))
 			continue
 	reference = pywikibot.Claim(site,'p143')
 	reference.setTarget(personal_name)
@@ -57,7 +59,7 @@ for line in lines:
 		item.addClaim(claim)
 		pywikibot.output(u'\03{{lightgreen}}{qid}: claim successfully added\03{{default}}'.format(qid=item.getID()))
 		claim.addSource(reference,bot=1)
-		pywikibot.output(u'\03{{lightgreen}}{qid}: source "personal name" successfully successfully added\03{{default}}'.format(qid=item.getID()))
+		pywikibot.output(u'\03{{lightgreen}}{qid}: source "personal name" successfully added\03{{default}}'.format(qid=item.getID()))
 		continue
 	item.get(force=True)
 	if prop in item.claims and len(item.claims[prop])==1 and item.claims[prop][0].getTarget().getID()==male.getID():
@@ -67,4 +69,4 @@ for line in lines:
 		except:
 			pass
 		continue
-	log('User:SamoaBot/sex conflicts',item,u'has "{{{{P|{propid}}}}} = {value}"'.format(propid=prop.replace('q',''),value=', '.join([u'{{{{Q|{qid}}}}}'.format(qid=claim.getTarget().getID()) for claim in item.claims[prop]])))
+	log('User:SamoaBot/sex conflicts',item,u' has "{{{{P|{propid}}}}} = {value}"'.format(propid=prop.replace('q',''),value=', '.join([u'{{{{Q|{qid}}}}}'.format(qid=claim.getTarget().getID().replace('q','')) for claim in item.claims[prop]])))
