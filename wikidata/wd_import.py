6b39be59bb5720c8846e63352b8b5225bc2e56dc
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-06-26 17:11:52 +0200

    finally working

--- wikidata/wd_import.py
+++ wikidata/wd_import.py
@@ -1,11 +1,28 @@
 # -*- coding: utf-8  -*-
 
+import re
 import string
 import pywikibot
 import mwparserfromhell
 from references import languages as reference_languages
 
 wd=pywikibot.Site('wikidata','wikidata').data_repository()
+
+def format_lccn(prev):
+	prev=prev.replace(' ','').replace('-','').replace('.','')
+	prev=re.sub(r'^http:\/\/lccn\.loc\.gov\/(.+)$','\g<1>',prev)
+	try:
+		prev=re.sub(r'(\w\/\d+)','\g<1>'+('0'*(6-len(re.search(r'\w\/\d+\/(\d+)',prev).group(1)))),prev,count=1)
+	except:
+		pass
+	prev=re.sub(r'\/','',prev)
+	try:
+		prev=re.sub(r'(?<=\d{2})','0'*(8-len(re.search(r'\d+',prev).group(0))),prev,count=1)
+	except:
+		pass
+	if re.match(ur'^\w+\d+$',prev):
+		return prev
+	return False
 
 harvesting=[
 	{
@@ -14,10 +31,12 @@ harvesting=[
 			{
 				'name':'VIAF',
 				'claims':'p214',
-				'remove':'itwiki'
+				'remove':'itwiki',
+				'filter':'^\d+$'
 			},{
 				'name':'LCCN',
 				'claims':'p244',
+				'filter':format_lccn,
 				'remove':'itwiki'
 			}
 		]
@@ -39,7 +58,33 @@ harvesting=[
 	}
 ]
 
-def from_page(page,import_data=True,remove=False):
+field_removal_summary={
+	'it':'[[Wikipedia:Bot|Bot]]: rimozione campi {} migrati a [[d:|Wikidata]]'
+}
+
+def remove_if(removed,template,field,value,text):
+	template_text=unicode(template)
+	if not template.has_param(field,False) and template.has_param(field.upper(),False):
+		field=field.upper()
+	if template.has_param(field,False):
+		try:
+			if unicode(template.get(field).value).strip()==value or (field=='LCCN' and format_lccn(unicode(template.get(field).value).strip())==value) or formatIMDb(unicode(template.get(field).value).strip(),'tt')==value or formatIMDb(unicode(template.get(field).value).strip(),'nm')==value or formatIMDb(unicode(template.get(field).value).strip(),'ch')==value or formatIMDb(unicode(template.get(field).value).strip(),'co')==value:
+				template.remove(field,force_no_field=True)
+				pywikibot.output(u'\03{{lightgreen}}field {} matches: removed\03{{default}}'.format(field))
+				removed.append(field)
+			elif unicode(template.get(field).value).strip()=='':
+				template.remove(field,force_no_field=True)
+				pywikibot.output(u'\03{{lightgreen}}field {} empty: removed\03{{default}}'.format(field))
+				removed.append(field)
+			else:
+				pywikibot.output(u'\03{{lightyellow}}field {} does not match: cannot be removed\03{{default}}'.format(field))
+		except:
+			pass
+	else:
+		pywikibot.output(u'\03{{lightyellow}}field {} not found in template\03{{default}}'.format(field))
+	return text.replace(template_text,unicode(template)),removed
+
+def from_page(page,import_data=True,remove=False,remove_exact=['VIAF','LCCN'],autosave=False):
 	pywikibot.output(u'parsing {page}'.format(page=page))
 	item=pywikibot.ItemPage.fromPage(page)
 	if not item.exists():
@@ -48,6 +93,7 @@ def from_page(page,import_data=True,remove=False):
 	text=page.get(force=True)
 	code=mwparserfromhell.parse(text)
 	imported=[]
+	removed=[]
 	for template in code.ifilter_templates():
 		tname=template.name.strip()
 		for harv in harvesting:
@@ -61,13 +107,21 @@ def from_page(page,import_data=True,remove=False):
 							rawvalue=value=unicode(template.get(pname).value)
 							pywikibot.output(u'\03{lightgreen}%s parameter found in %s: %s\03{default}'%(pname,tname,value))
 							if 'filter' in param:
-								for func in (param['filter'] if isinstance(param['filter'],list) else [param['filter']]):
-									value=func(value)
+								for filtr in (param['filter'] if isinstance(param['filter'],list) else [param['filter']]):
+									if callable(filtr):
+										value=filtr(value)
+									else:
+										match=re.search(filtr,value)
+										if match:
+											value=match.group(0)
+										else:
+											pywikibot.output(u'\03{{lightyellow}}{} value was skipped because it is excluded by filter\03{{default}}'.format(value))
+											continue
 							if 'map' in param:
 								if value in param['map']:
 									value=param['map'][value]
 								else:
-									pywikibot.output(u'\03{lightyellow}%s value was skipped because it is not mapped\03{default}'%value)
+									pywikibot.output(u'\03{{lightyellow}}{} value was skipped because it is not mapped\03{{default}}'.format(value))
 									continue
 							if value!=rawvalue:
 								pywikibot.output(u'{pname} parameter formatted from {frm} to {to}'.format(pname=pname,frm=rawvalue,to=value))
@@ -96,14 +150,25 @@ def from_page(page,import_data=True,remove=False):
 												imported.append(prop)
 											except:
 												pass
+							if remove and (isinstance(param['claims'],basestring) or len(param['claims'])==1):
+								prop=(param['claims'] if isinstance(param['claims'],basestring) else param['claims'][0])
+								if len(item.claims[prop])==1:
+									page.text,removed=remove_if(removed,template,pname,item.claims[prop][0].getTarget(),page.text)
 							break
+						elif template.has_param(pname,False):
+							page.text,removed=remove_if(removed,template,pname,'',page.text)
 				break
-	return imported
+	if remove and removed and (not remove_exact or len(list(set(removed)-set(remove_exact)))==0):
+		pywikibot.showDiff(text,page.text)
+		if autosave or pywikibot.inputChoice(page.title(),['Yes', 'No'],['Y', 'N'],'N').strip().lower() in ['yes','y']:
+			page.save(comment=field_removal_summary[page.site.lang].format(', '.join(removed)),minor=True,botflag=True)
+	return (imported,removed)
 
 if __name__=='__main__':
 	total=None
 	import_data=True
 	remove=False
+	autosave=False
 	site=pywikibot.Site()
 	cat=None
 	recurse=None
@@ -114,8 +179,12 @@ if __name__=='__main__':
 			total=int(arg[7:])
 		elif arg.startswith('-noimport'):
 			import_data=False
+		elif arg.startswith('-import:'):
+			import_data=[arg[8:]]
 		elif arg.startswith('-remove'):
 			remove=True
+		elif arg.startswith('-autosave'):
+			autosave=True
 		elif arg.startswith('-wiki:'):
 			site=pywikibot.Site(arg[6:],'wikipedia')
 		elif arg.startswith('-cat:'):
@@ -134,4 +203,4 @@ if __name__=='__main__':
 	if cat:
 		pages=pages+list(pywikibot.Category(site,cat).articles(namespaces=0,startsort=start,total=total,recurse=recurse))
 	for page in sorted([page for page in list(set(pages)) if page.title()>=start]):
-		from_page(page,import_data=import_data,remove=remove)
+		from_page(page,import_data=import_data,remove=remove,autosave=autosave)
