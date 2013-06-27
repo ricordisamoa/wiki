a145580626575abf16d39263a0fc0e9f84d1e1da
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-06-27 17:51:51 +0200

    basic script to help finding duplicate stub-icons on itwiki

---
+++ stub_icons.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8  -*-
+
+import pywikibot
+
+site=pywikibot.Site('it','wikipedia')
+
+for image in site.allimages(prefix='Stub ',total=100):
+	data=pywikibot.data.api.Request(site=site,action='query',titles=image.title(),prop='duplicatefiles').submit()['query']['pages']
+	for info in data:
+		if 'duplicatefiles' in data[info]:
+			for df in data[info]['duplicatefiles']:
+				if 'shared' in df:
+					pywikibot.output(u'\03{{lightyellow}}shared duplicate found for {local}: {shared}\03{{default}}'.format(local=image.title(),shared=df['name']))
+		else:
+			pywikibot.output(u'\03{{lightblue}}no duplicate found for {local}\03{{default}}'.format(local=image.title()))
