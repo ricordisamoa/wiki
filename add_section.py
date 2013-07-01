d587237b9f11f3c6c69ab6ea28783a8ed44aff9b
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-07-01 03:23:12 +0200

    not zero-padded date by default

--- add_section.py
+++ add_section.py
@@ -22,8 +22,8 @@ def main():
 				del kwargs['title']
 				insert(**kwargs)
 
-def insert(page,level=2,space=True,minorEdit=True):
-	header=pywikibot.data.api.Request(site=page.site,action='expandtemplates',text='{{#timel:d xg}}').submit()['expandtemplates']['*']
+def insert(page,level=2,space=True,zero_padded=False,minorEdit=True):
+	header=pywikibot.data.api.Request(site=page.site,action='expandtemplates',text='{{#timel:%s xg}}'%('d' if zero_padded else 'j')).submit()['expandtemplates']['*']
 	text=page.get(force=True)
 	if pywikibot.textlib.does_text_contain_section(text,header):
 		pywikibot.output(u'\03{{lightyellow}}{page} already contains the header for "{header}"\03{{default}}'.format(page=page,header=header))
