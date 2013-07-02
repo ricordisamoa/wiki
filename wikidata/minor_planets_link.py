7e9cfe1d2686a949bc03894f925caf7a1a166e40
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-07-02 09:46:14 +0200

    set up integration with nlwiki, fywiki

--- wikidata/minor_planets_link.py
+++ wikidata/minor_planets_link.py
@@ -3,43 +3,49 @@
 import re
 import pywikibot
 
-pywikibot.handleArgs()
+cat={
+	'en':u'Lists of minor planets by number',
+	'fy':u'List fan planetoïden',
+	'nl':u'Planetoïdenlijst'
+}
+fmt={
+	'en':u'List of minor planets/{}–{}',
+	'fy':u'List fan planetoïden {}-{}',
+	'nl':u'Lijst van planetoïden {}-{}'
+}
 
-site = pywikibot.Site('wikidata','wikidata').data_repository()
-site.login()
+def main(site1=pywikibot.Site('fy','wikipedia'),site2=pywikibot.Site('en','wikipedia')):
+	site1.data_repository().login()
+	regex=re.escape(fmt[site1.lang]).replace('\{','{').replace('\}','}').format('(\d+)','(\d+)')
+	for page1 in pywikibot.Category(site1,cat[site1.lang]).articles():
+		match = re.match(regex,page1.title())
+		if not match:
+			continue
+		print page1.title()
+		print match.group(1),match.group(2)
+		page2 = pywikibot.Page(site2,fmt[site2.lang].format(match.group(1),match.group(2)))
+		print page2.title()
+		if not page2.exists():
+			continue
+		item1 = pywikibot.ItemPage.fromPage(page1)
+		item2 = pywikibot.ItemPage.fromPage(page2)
+		if item2.exists():
+			if not item1.exists():
+				try:
+					item2.get(force=True)
+					newdata = {'sitelinks':{}}
+					newdata['sitelinks'][site1.dbName()] = {'site':site1.dbName(),'title':page1.title()}
+					if not site1.lang in item2.labels:
+						newdata['labels'] = {}
+						newdata['labels'][site1.lang] = {'language':site1.lang,'value':page1.title()}
+					item2.editEntity(newdata)
+					pywikibot.output(u'\03{lightgreen}editEntity successful\03{default}')
+				except:
+					pywikibot.output(u'\03{lightred}editEntity failed\03{default}')
+			elif item1!=item2:
+				pywikibot.output(u'\03{{lightyellow}}{} and {} should be merged\03{{default}}'.format(item1.getID(),item2.getID()))
+				# TODO: call merge.py
 
-nlwiki = pywikibot.Site('nl','wikipedia')
-enwiki = pywikibot.Site('en','wikipedia')
-
-nlcat = u'Planetoïdenlijst'
-nlmatch = ur'^Lijst van planetoïden (\d+)[\-\–](\d+)$'
-enformat = u'List of minor planets/{}–{}'
-
-for nlpage in pywikibot.Category(nlwiki,nlcat).articles():
-	match = re.match(nlmatch,nlpage.title())
-	if not match:
-		continue
-	print nlpage.title()
-	print match.group(1),match.group(2)
-	enpage = pywikibot.Page(enwiki,enformat.format(match.group(1),match.group(2)))
-	print enpage.title()
-	if not enpage.exists():
-		continue
-	nlitem = pywikibot.ItemPage.fromPage(nlpage)
-	enitem = pywikibot.ItemPage.fromPage(enpage)
-	if enitem.exists():
-		if nlitem.exists():
-			# TODO: compare and merge the two items
-			pass
-		else:
-			try:
-				enitem.get(force=True)
-				newdata = {'sitelinks':{}}
-				newdata['sitelinks'][nlwiki.dbName()] = {'site':nlwiki.dbName(),'title':nlpage.title()}
-				if not nlwiki.lang in enitem.labels:
-					newdata['labels'] = {}
-					newdata['labels'][nlwiki.lang] = {'language':nlwiki.lang,'value':nlpage.title()}
-				enitem.editEntity(newdata)
-				pywikibot.output('\03{lightgreen}editEntity successful\03{default}')
-			except:
-				pywikibot.output('\03{lightred}editEntity failed\03{default}')
+if __name__=='__main__':
+	pywikibot.handleArgs()
+	main()
