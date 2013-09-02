eb80846daa350a6defbec7a3527043e1b11cc15f
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-09-02 09:06:47 +0200

    restricting claim replacing

--- wikidata/delinker.py
+++ wikidata/delinker.py
@@ -31,14 +31,18 @@ def main(commands='User:SamoaBot/Delinker/commands'):
 			if not template.has_param('by'):
 				pywikibot.output(yellow%'no assignee specified')
 				continue
+			if template.has_param('prop'):
+				prop_only=unicode(template.get('prop').value).lower().replace('p','').split('-')
+				pywikibot.output(u'\03{{lightpurple}}restricting claim replacing to the following properties: {props}'.format(props=', '.join(prop_only)))
+			else:
+				prop_only=None
 			itemfrom=pywikibot.ItemPage(wd,'Q'+unicode(template.get('from').value))
 			itemto=pywikibot.ItemPage(wd,'Q'+unicode(template.get('to').value))
-			summary=u'[[User:SamoaBot/Delinker|SamoaBot Delinker]]: migrating [[{itemfrom}]] to [[{itemto}]]'.format(itemfrom=itemfrom.getID().upper(),itemto=itemto.getID().upper())
-			status=None
+			summary=u'[[User:SamoaBot/Delinker|SamoaBot Delinker]]: migrating [[{itemfrom}]] to [[{itemto}]] as ordered by [[User:{by}|{by}]] {reason}'.format(itemfrom=itemfrom.getID().upper(),itemto=itemto.getID().upper(),by=unicode(template.get('by').value),reason=unicode(template.get('reason').value))
 			count=0
 			refs=list(itemfrom.getReferences(namespaces=[0]))
 			if len(refs)==0:
-				pywikibot.output(yellow%(u'%s has no references'%itemfrom.getID()))
+				pywikibot.output(yellow%(u'{} has no references'.format(itemfrom)))
 				template.add('status','nothingtodo')
 			else:
 				count=0
@@ -46,18 +50,20 @@ def main(commands='User:SamoaBot/Delinker/commands'):
 					item=pywikibot.ItemPage(wd,page.title())
 					item.get(force=True)
 					for prop in item.claims:
+						if prop_only and (not prop.lower().replace('p','') in prop_only):
+							continue
 						for claim in item.claims[prop]:
 							if claim.getTarget()==itemfrom:
 								claim.changeTarget(itemto,summary=summary)
 								item.get(force=True)
-								pywikibot.output(u'\03{lightgreen}changed claim target\03{default}')
+								pywikibot.output(u'\03{{lightgreen}}{item}: changed claim target'.format(item=item))
 								count+=1
-				if count and count==len(refs) and len(list(itemfrom.getReferences(namespaces=[0])))==0:
-					pywikibot.output(u'\03{lightgreen}delinking successful for %s\03{default}'%itemfrom.getID())
+				if prop_only or (count and count==len(refs) and len(list(itemfrom.getReferences(namespaces=[0])))==0):
+					pywikibot.output(u'\03{{lightgreen}}delinking successful for {}'.format(itemfrom))
 					template.add('status','done')
 					template.add('items',str(count))
 				else:
-					pywikibot.output('\03{lightred}delinking unsuccessful for %s\03{default}'%itemfrom.getID())
+					pywikibot.output('\03{{lightred}}delinking unsuccessful for {}'.format(itemfrom))
 					template.add('status','error')
 	newtext=unicode(code)
 	if newtext!=text:
