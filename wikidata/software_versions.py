6d916ed291da4318fb7e881fe6061733c6c620d4
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-09-08 15:04:28 +0200

    use edit summary in Claim.changeTarget and ItemPage.addClaim; minor fixes

--- wikidata/software_versions.py
+++ wikidata/software_versions.py
@@ -24,29 +24,32 @@ items = {
 for qid in items:
 	item = pywikibot.ItemPage(site,qid)
 	if not item.exists():
-		pywikibot.output('\03{{lightyellow}}Warning: item {item} does not exist\03{{default}}'.format(item=item))
+		pywikibot.output(u'\03{{lightyellow}}Warning: item {item} does not exist\03{{default}}'.format(item=item))
 		continue
 	item.get(force=True)
 	for prop in items[qid]:
+		propname=pywikibot.PropertyPage(site,prop)
+		propname.get()
+		propname=propname.labels['en']
 		new = items[qid][prop][1](BeautifulSoup(urllib2.urlopen(items[qid][prop][0]).read()))
-		pywikibot.output('latest version for "{program}" according to "{site}": {new}'.format(program=item.labels['en'],site=items[qid][prop][0],new=new))
+		pywikibot.output(u'{prop} for "{program}" according to "{url}": {new}'.format(prop=propname,program=item.labels['en'],url=items[qid][prop][0],new=new))
 		if prop in item.claims:
 			if len(item.claims[prop])==1:
 				old = item.claims[prop][0].getTarget()
 				comp = items[qid][prop][2]
 				if comp(new) > comp(old):
-					pywikibot.output('\03{{lightblue}}{item} has an out-of-date claim for {prop}: "{old}" instead of "{new}"\03{{default}}'.format(item=item,prop=prop,old=old,new=new))
-					item.claims[prop][0].changeTarget(new)
-					pywikibot.output('\03{{lightgreen}}claim updated in {item} for {prop} with value "{val}"\03{{default}}'.format(item=item,prop=prop,val=new))
+					pywikibot.output(u'\03{{lightblue}}{program} has an out-of-date claim for {prop}: "{old}" instead of "{new}"\03{{default}}'.format(program=item.labels['en'],prop=propname,old=old,new=new))
+					item.claims[prop][0].changeTarget(new,summary=u'updating {prop} from {url}'.format(prop=propname,url=items[qid][prop][0]))
+					pywikibot.output(u'\03{{lightgreen}}claim updated in {program} for {prop} with value "{val}"\03{{default}}'.format(program=item.labels['en'],prop=propname,val=new))
 				elif comp(new) == comp(old):
-					pywikibot.output('\03{{lightblue}}{item} has an updated claim for {prop}: "{old}" == "{new}"\03{{default}}'.format(item=item,prop=prop,old=old,new=new))
+					pywikibot.output(u'\03{{lightblue}}{program} has an up-to-date claim for {prop}: "{old}" == "{new}"\03{{default}}'.format(program=item.labels['en'],prop=propname,old=old,new=new))
 				else:
-					pywikibot.output('\03{{lightyellow}}Warning: {item} has a greater claim for {prop}: "{old}" > "{new}" \03{{default}}'.format(item=item,prop=prop,old=old,new=new))
+					pywikibot.output(u'\03{{lightyellow}}Warning: {program} has a greater claim for {prop}: "{old}" > "{new}" \03{{default}}'.format(program=item.labels['en'],prop=propname,old=old,new=new))
 			else:
-				pywikibot.output('\03{{lightyellow}}Warning: {item} contains {num} claims for {prop}\03{{default}}'.format(item=item,num=len(item.claims[prop]),prop=prop))
+				pywikibot.output(u'\03{{lightyellow}}Warning: {item} contains {num} claims for {prop}\03{{default}}'.format(item=item,num=len(item.claims[prop]),prop=propname))
 		else:
-			pywikibot.output('\03{{lightgreen}}{item} does not contain any claim for {prop}\03{{default}}'.format(item=item,prop=prop))
+			pywikibot.output(u'\03{{lightgreen}}{program} does not contain any claim for {prop}\03{{default}}'.format(program=item.labels['en'],prop=propname))
 			claim = pywikibot.Claim(site,prop)
 			claim.setTarget(new)
-			item.addClaim(claim)
-			pywikibot.output('\03{{lightgreen}}claim added to {item} for {prop} with value "{val}"\03{{default}}'.format(item=item,prop=prop,val=new))
+			item.addClaim(claim,summary=u'importing {prop} from {url}'.format(prop=propname,url=items[qid][prop][0]))
+			pywikibot.output(u'\03{{lightgreen}}claim added to {program} for {prop} with value "{val}"\03{{default}}'.format(item.labels['en'],prop=propname,val=new))
