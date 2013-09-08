f6761adeb46163d874d54fb0b565ea7d16ff9989
Ricordisamoa <ricordisamoa@openmailbox.org>
2013-09-08 03:24:22 +0200

    basic script to remove 'duplicate' VIAF entries (SamoaBot 38)

---
+++ wikidata/viaf_redir.py
@@ -0,0 +1,35 @@
+# -*- coding: utf-8  -*-
+
+import re
+import urllib2
+import pywikibot
+
+viaf='p214'
+baseurl='http://viaf.org/viaf/{}'
+regex=ur'http\:\/\/viaf\.org\/viaf\/(\d+)\/?'
+site=pywikibot.Site().data_repository()
+
+viols=pywikibot.Page(site,'Database reports/Constraint violations/'+viaf.upper(),ns=4).get(force=True)
+for match in re.finditer(ur'^\*\s*\[\[(?P<item>[Qq]\d+)\]\]\:\s*\['+regex+'\s+\d+\](\,\s*\['+regex+'\s+\d+\])+(\n|$)',viols,flags=re.MULTILINE):
+	item=pywikibot.ItemPage(site,match.group('item'))
+	item.get(force=True)
+	if viaf in item.claims and len(item.claims[viaf])>1:
+		removable=[]
+		values=[c.getTarget() for c in item.claims[viaf]]
+		for claim in item.claims[viaf]:
+			prev=claim.getTarget()
+			url=baseurl.format(prev)
+			got=re.match(regex+'$',urllib2.urlopen(url).geturl()).group(1)
+			if got!=prev:
+				pywikibot.output(u'\03{{lightblue}}searched VIAF for {prev} but got redirected to {got}'.format(prev=prev,got=got))
+				if got in values:
+					pywikibot.output(u'\03{{lightgreen}}claim pertaining to VIAF {prev} is eligible for removal'.format(prev=prev))
+					removable.append(claim)
+				else:
+					pywikibot.output(u'\03{{lightyellow}}VIAF {got} was not found in existing claims, so {prev} cannot be removed'.format(prev=prev,got=got))
+		if len(removable)>0:
+			s=('' if len(removable)==1 else 's')
+			item.removeClaims(removable,summary='removed redirected VIAF id{s}'.format(s=s))
+			pywikibot.output(u'\03{{lightgreen}}{num} claim{s} removed from {item}'.format(num=len(removable),s=s,item=item))
+		else:
+			pywikibot.output(u'\03{{lightyellow}}no removable VIAF ids found on {item}'.format(item=item))
