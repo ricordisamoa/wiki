a821efab0303a934675b7677764155053d882aab
Ricordisamoa <ricordisamoa@openmailbox.org>
2014-03-15 19:20:27 +0100

    basic script to update the translated edit summary used for Wikidata migration

---
+++ summary_update.py
@@ -0,0 +1,33 @@
+# -*- coding: utf-8  -*-
+# released as CC0 — Creative Commons Public Domain Dedication:
+# To the extent possible under law, the person who associated CC0 with this work
+# has waived all copyright and related or neighboring rights to this work.
+
+import pywikibot
+
+meta = pywikibot.Site('meta', 'meta')
+summary = []
+kwargs = {
+	'namespace': 1198,
+	'prefix': 'User:SamoaBot/Wikidata Summary/translate/1/',
+	'filterredir': False
+}
+pages = list(meta.allpages(**kwargs))
+kwargs['prefix'] = kwargs['prefix'].replace('/1/', '/2/')
+pages = pages + list(meta.allpages(**kwargs))
+
+for page in pages:
+	text = page.get(force=True)
+	
+	# to make it compatible with pywikibot.i18n
+	text = text.replace('{{plural:', '{{PLURAL:')
+	text = text.replace('$counter', '%(counter)d').replace('$user', '%(user)s').replace('$params', '%(params)s').replace('$id', '%(id)s')
+	
+	spl = page.title().split('/')
+	num = int(spl[-2])
+	while num > len(summary):
+		summary.append({})
+	summary[num - 1][spl[-1]] = text
+
+with open('wikidata_summary.py', 'w') as f:
+	f.write(u'summary = ' + str(summary))
